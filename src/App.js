6284102490ebac7e1efe2501d745d81bab2605f2
minhnd-0256 <nghiem.dinh.minh@sun-asterisk.com>
2022-08-27 21:08:44 +0700

    Update radio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import './styles.css'
 
 function App() {
+  const [prefCode, setPrefCode] = useState([]);
+  const [prefID, setPrefID] = useState(-1);
+  const [data,setData] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      await axios
+        .get(`https://opendata.resas-portal.go.jp/api/v1/prefectures`, {
+          headers: {
+            "X-API-KEY": "OrG2P6cuTztfnrAd0nvC9NK1Bsabel1QyFJOaBL8",
+            Accept: "application/json",
+          },
+        })
+        .then((res) => {
+          setPrefCode(res.data.result);
+        });
+    };
+    getData();
+  }, []);
+
+  const renderCheckList = () => {
+    return prefCode?.map((item) => {
+      return (
+        <div key={item.prefCode}>
+          <input value={item.prefCode} type="radio" onChange={handleCheck} name="prefCode" />
+          <span>{item.prefName}</span>
+        </div>
+      );
+    });
+  };
+
+  const handleCheck = (event) => {
+    if (event.target.checked) {
+      setPrefID(event.target.value);
+      axios.get(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${event.target.value}`
+      , {
+      headers: {
+        'X-API-KEY': 'OrG2P6cuTztfnrAd0nvC9NK1Bsabel1QyFJOaBL8',
+        'Accept': 'application/json',
+      }
+      }).then((res) => {
+        setData(res.data.result.data[0].data)
+      })  
+      }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <tr>
+            <td>
+              <LineChart
+                width={1000}
+                height={700}
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="year" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
+              </LineChart>
+            </td>
+            <td></td>
+            <td>
+              <div className="app">
+                <div className="checkList">
+                  <div className="title">Choose the area:</div>
+                  <div className="list-container">
+                    {renderCheckList()}
+                  </div>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </table>
       </header>
     </div>
   );
 }
-
 export default App;
