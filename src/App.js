a79f9dd52e777b5d996aa5f6b6bc67bd5571e836
minhnd-0256 <nghiem.dinh.minh@sun-asterisk.com>
2022-08-28 13:36:04 +0700

    Update css

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
-import './styles.css'
+import "./styles.css";
 
 function App() {
   const [prefCode, setPrefCode] = useState([]);
   const [prefID, setPrefID] = useState(-1);
-  const [data,setData] = useState([])
-  const [prefName,setPrefName] = useState([])
-  const text1 = "Population of "
+  const [data, setData] = useState([]);
+  const [prefName, setPrefName] = useState([]);
+  const text1 = "Population of ";
 
   useEffect(() => {
     const getData = async () => {
@@ -36,35 +36,43 @@ function App() {
 
   const renderCheckList = () => {
     return (
-    <div className="container">
-      {
-        prefCode?.map((item) => {
+      <div className="container">
+        {prefCode?.map((item) => {
           return (
             <div key={item.prefCode}>
-            <input value={item.prefCode} type="radio" onChange={handleCheck} name="prefCode" id={item.prefName} />
-            <label htmlFor={item.prefName}>{item.prefName}</label>
+              <input
+                value={item.prefCode}
+                type="radio"
+                onChange={handleCheck}
+                name="prefCode"
+                id={item.prefName}
+              />
+              <label htmlFor={item.prefName}>{item.prefName}</label>
             </div>
           );
-        })
-      }
-    </div>
-    )
+        })}
+      </div>
+    );
   };
 
   const handleCheck = (event) => {
     if (event.target.checked) {
-      setPrefName(event.target.id)
+      setPrefName(event.target.id);
       setPrefID(event.target.value);
-      axios.get(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${event.target.value}`
-      , {
-      headers: {
-        'X-API-KEY': 'OrG2P6cuTztfnrAd0nvC9NK1Bsabel1QyFJOaBL8',
-        'Accept': 'application/json',
-      }
-      }).then((res) => {
-        setData(res.data.result.data[0].data)
-      })  
-      }
+      axios
+        .get(
+          `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${event.target.value}`,
+          {
+            headers: {
+              "X-API-KEY": "OrG2P6cuTztfnrAd0nvC9NK1Bsabel1QyFJOaBL8",
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setData(res.data.result.data[0].data);
+        });
+    }
   };
   return (
     <div className="App">
@@ -85,10 +93,15 @@ function App() {
               >
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="year" />
-                <YAxis />
+                <YAxis ticks={[1000000,2000000,3000000,4000000, 5000000, 7000000, 9000000,12000000,15000000]}   />
                 <Tooltip />
                 <Legend />
-                <Line name={text1.concat(prefName)} dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line
+                  name={text1.concat(prefName)}
+                  dataKey="value"
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                />
               </LineChart>
             </td>
             <td></td>
@@ -96,9 +109,7 @@ function App() {
               <div className="app">
                 <div className="checkList">
                   <div className="title">Choose the area:</div>
-                  <div className="list-container">
-                    {renderCheckList()}
-                  </div>
+                  <div className="list-container">{renderCheckList()}</div>
                 </div>
               </div>
             </td>
